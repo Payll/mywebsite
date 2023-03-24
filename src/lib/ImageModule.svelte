8e464ae1edbe947d021ee23ebaf88aac3e0b9d4b
Payll <yann.paillard@laposte.net>
2023-03-24 15:52:01 +0100

    integrate compo

---
+++ src/lib/ImageModule.svelte
@@ -0,0 +1,17 @@
+<!-- ImageModule.svelte -->
+<script>
+  export let src;
+  export let alt;
+  export let width;
+  export let height;
+</script>
+
+{#if src.includes('.svg')}
+  <svg {width} {height}>
+    <use xlink:href={src} class=""/>
+  </svg>
+{:else}
+  {#if src !== ""}
+    <img src={src} alt={alt} width={width} {height}>
+  {/if}
+{/if}
