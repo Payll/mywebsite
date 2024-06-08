7039a54aa0209dc6977ab10a06c31c569c5714c1
Yann <yann.paillard@laposte.net>
2024-06-08 20:33:59 +0200

    V0.0.1

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,56 +1,87 @@
 <script>
+    export let sidebarExpanded = false;
 </script>
 
-<!-- Nav Bar -->
-<div class="navbar bg-black ">
-    
+<style>
+    @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+</style>
+
+
+<div class="navbar flex flex-col">
+    <!-- Collapse Button -->
+    <div class="mt-2 mb-2 flex items-cente">
+        <div class="divider text-white text-xl font-bold flex items-center">
+            <span class="material-icons">menu</span>
+        </div>
+    </div>
+
     <!-- Home Button -->
-    <div class="mr-auto mt-2 mb-2  flex">
-        <a class="btn ease-in duration-300 bg-transparent" href="/" rel="noreferrer noopener" >
-          <div class = "divider text-white" id=fadeshow1>Home</div>
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300 w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="/" rel="noreferrer noopener">
+            <span class="material-icons text-white text-xl">home</span>
+            {#if sidebarExpanded}
+                <span class="ml-2">Home</span>
+            {/if}
         </a>
     </div>
 
-    <!-- Education Button -->
-    <div class="mt-2 mb-2 flex ml-2">
-        <a class="btn ease-in duration-300  bg-transparent" href="/education" rel="noreferrer noopener">
-            <div class = "divider text-white" id=fadeshow1>Education & Job</div>
+    <!-- Education & Jobs Button -->
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300 w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="/education" rel="noreferrer noopener">
+            <span class="material-icons text-white text-xl">work</span>
+            {#if sidebarExpanded}
+                <span class="ml-2">Education & Jobs</span>
+            {/if}
         </a>
     </div>
 
     <!-- Projects Button -->
-    <div class="mt-2 mb-2 flex ml-2">
-        <a class="btn ease-in duration-300  bg-transparent" href="/projects" rel="noreferrer noopener">
-            <div class = "divider text-white" id=fadeshow1>Projects</div>
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300 w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="/projects" rel="noreferrer noopener">
+            <span class="material-icons text-white text-xl">code</span>
+            {#if sidebarExpanded}
+                <span class="ml-2">Projects</span>
+            {/if}
         </a>
     </div>
 
     <!-- Skills Button -->
-    <div class="mt-2 mb-2 flex ml-2">
-        <a class="btn ease-in duration-300  bg-transparent" href="/skills" rel="noreferrer noopener">
-            <div class = "divider text-white" id=fadeshow1>Skills</div>
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300  w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="/skills" rel="noreferrer noopener">
+            <span class="material-icons text-white text-xl">build</span>
+            {#if sidebarExpanded}
+                <span class="ml-2">Skills</span>
+            {/if}
         </a>
     </div>
 
     <!-- About Button -->
-    <div class="mt-2 mb-2 flex ml-2">
-        <a class="btn ease-in duration-300  bg-transparent" href="/about" rel="noreferrer noopener">
-            <div class = "divider text-white" id=fadeshow1>About</div>
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300  w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="/about" rel="noreferrer noopener">
+            <span class="material-icons text-white text-xl">info</span>
+            {#if sidebarExpanded}
+                <span class="ml-2">About</span>
+            {/if}
         </a>
     </div>
 
     <!-- Linkedin Button -->
-    <div class="mt-2 mb-2 flex ml-2">
-        <a class="btn ease-in duration-300  bg-transparent" href="https://www.linkedin.com/in/yann-paillard/?locale=en_US" rel="noreferrer noopener">
-            <div class = "divider text-white" id=fadeshow1>Linkedin</div>
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300  w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="https://www.linkedin.com/in/yann-paillard/?locale=en_US" rel="noreferrer noopener" target="_blank">
+            <img src="linkedin-box-fill.svg" class="h-6 w-6" alt="LinkedIn">
+            {#if sidebarExpanded}
+                <span class="ml-2">LinkedIn</span>
+            {/if}
         </a>
     </div>
 
     <!-- Github Button -->
-    <div class="mt-2 mb-2 flex ml-2">
-        <a class="btn ease-in duration-300  bg-transparent" href="https://github.com/Payll" rel="noreferrer noopener">
-            <img class="rounded-md h-10 w-10" src="https://avatars.githubusercontent.com/u/74405680?v=4" alt="Yann Paillard">
+    <div class="mt-2 mb-2 flex items-center hover:scale-110 transition duration-300  w-full">
+        <a class="ease-in duration-300 bg-transparent flex items-center" href="https://github.com/Payll" rel="noreferrer noopener" target="_blank">
+            <span class="material-icons text-white text-xl">code</span>
+            {#if sidebarExpanded}
+                <span class="ml-2">GitHub</span>
+            {/if}
         </a>
     </div>
-
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,147 @@
     import "../app.css";
     import Nav from "../lib/Nav.svelte";
     import Footer from "../lib/Footer.svelte";
+    import { onMount } from 'svelte';
+
+    let sections = [];
+    let currentActive = '';
+    let sidebarExpanded = false;
+
+    onMount(() => {
+        sections = Array.from(document.querySelectorAll('.my-section'));
+        let ratios = {};
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    ratios[entry.target.id] = entry.intersectionRatio;
+                });
+
+                let maxRatio = 0;
+                let maxSection = '';
+                for (const [section, ratio] of Object.entries(ratios)) {
+                    if (ratio > maxRatio) {
+                        maxRatio = ratio;
+                        maxSection = section;
+                    }
+                }
+
+                currentActive = maxSection; // Updating the active section
+            }, {
+                rootMargin: '0px 0px -40% 0px', // Adjusted root margin
+                threshold: [0, 0.5, 1]
+            }
+        );
+
+        sections.forEach(section => observer.observe(section));
+
+        // Fallback for when user scrolls to the bottom
+        window.addEventListener('scroll', () => {
+            const scrollTotal = document.documentElement.scrollHeight - window.innerHeight;
+            if ((window.scrollY >= scrollTotal) && sections.length) {
+                currentActive = sections[sections.length - 1].id; // Set the last section as active
+            }
+        });
+    });
+
+    function handleMouseEnter() {
+        sidebarExpanded = true;
+
+    }
+
+    function handleMouseLeave() {
+        sidebarExpanded = false;
+    }
 </script>
 
-<Nav></Nav>
-<slot />
-<Footer></Footer>
+<style>
+    /* CSS for the dots */
+    .nav-dot {
+        transition: transform 0.3s;
+        font-size: 1rem; /* Normal size */
+    }
+
+    .nav-dot.active {
+        transform: scale(1.5); /* Larger when active */
+        font-size: 1.5rem; /* or any other transformation you want */
+        margin-bottom: 5px;
+        margin-top: 5px;
+        transition: transform 0.3s;
+        color: #ffffff;
+    }
+
+    /* CSS for the animated dots */
+    .animated-dot {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background-color: #ffffff;
+        margin: 5px;
+        animation: bounce 1s infinite;
+    }
+
+    .animated-dot:nth-child(2) {
+        animation-delay: 0.1s;
+    }
+
+    .animated-dot:nth-child(3) {
+        animation-delay: 0.2s;
+    }
+
+    .animated-dot:nth-child(4) {
+        animation-delay: 0.3s;
+    }
+
+    .animated-dot:nth-child(5) {
+        animation-delay: 0.4s;
+    }
+
+    @keyframes bounce {
+        0%, 100% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(-10px);
+        }
+    }
+
+    /* Background for the sidebar when expanded */
+    .sidebar-expanded-background {
+        background-color: rgba(38, 43, 50, 0.9); /* Example background color */
+    }
+</style>
+
+<div class="flex h-full">
+    <!-- Left Sidebar -->
+    <div
+       class="fixed top-0 left-0 h-screen border-2 border-gray-500 rounded-l-md shadowRight z-10 transition-all duration-300 {sidebarExpanded ? 'w-40 sidebar-expanded-background' : 'w-10'}"
+       on:mouseenter={() => sidebarExpanded = true}
+       on:mouseleave={() => sidebarExpanded = false}
+    >
+       <Nav {sidebarExpanded}></Nav>
+   </div>
+
+    <!-- Main Content -->
+    <div class="flex-col flex-1 h-full pl-14 pr-5 md:pl-14 md:pr-14 pt-10">
+        <slot />
+        <Footer></Footer>
+    </div>
+
+    <!-- Right Sidebar with animation -->
+<div class="fixed top-0 right-0 h-screen w-10 border-2 border-gray-500 rounded-r-md z-10 hidden md:block">
+    <div class="flex flex-col items-center justify-center h-1/2">
+        <div class="animated-dot"></div>
+        <div class="animated-dot"></div>
+        <div class="animated-dot"></div>
+        <div class="animated-dot"></div>
+        <div class="animated-dot"></div>
+    </div>
+
+    <div class="flex flex-col items-center justify-center h-1/2">
+        {#each sections as section (section.id)}
+            <a href={`#${section.id}`} class="nav-dot {currentActive === section.id ? 'active' : ''}">&#8226;</a>
+        {/each}
+    </div>
+</div>
+
+</div>
